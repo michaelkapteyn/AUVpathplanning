c88477c48314db3740351c54b8e4326d907c8a69
MichaelSchmid1 <47392288+MichaelSchmid1@users.noreply.github.com>
2019-04-02 17:50:59 -0400

    Update mission.py

--- mission.py
+++ mission.py
@@ -3,13 +3,13 @@ from shapely.geometry.polygon import Polygon
 from numpy import dtype
 from Astar import astar
 
+# class for defining an underwater exploration mission
 class MISSION:
     def __init__(self, discretization_distance=1, worldsize_x=100, worldsize_y=100):
         """
-        discretization distance in meters
-        worldsizes in meters
+        discretization_distance = discretization distance in meters
+        worldsize_x, worldsize_y = worldsizes in meters
         """
-        # TODO: Reduce to square exploration area (only one self.worldsize)
         self.worldsize_x = worldsize_x
         self.worldsize_y = worldsize_y
         self.discretization = int(round(worldsize_x / discretization_distance))+1
